f79797503115e339a962240113adb0512eb7a004
phita <pablo.hitalara@usp.ceu.es>
2024-10-10 13:29:44 +0200

    Server and signal

--- src/Patient.java
+++ src/Patient.java
@@ -1,2 +1,11 @@
+
+import java.time.LocalDate;
+
 public class Patient {
+    private String name;
+    private String surname;
+    private LocalDate dob;
+    private String email;
+    private Signal Singal;
+
 }
